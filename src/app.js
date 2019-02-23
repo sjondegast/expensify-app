eeed2f29e65588740141e5bd4c5b8b9bd516e31d
sjon de gast <>
2019-02-23 20:06:41 +0100

    app.js added edit and help components, routes react-router and react-router Link to link between them

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
@@ -8,17 +8,46 @@ const ExpenseDashboardPage = () => (
   <div>This is from my ExpenseDashboardPage</div>
 );
 
-const AddExpensePage = () => (
-    <div>Add expense page</div>
-  );
+const AddExpensePage = () => <div>Add expense page</div>;
+
+const EditExpensePage = () => (
+    <div>
+        This is from my edit page
+    </div>
+);
+
+const HelpPage = () => (
+    <div>
+        This is my help page
+    </div>
+);
 
 const routes = (
-  <BrowserRouter>
+  <Router>
     <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/create">Add Expense</Link>
+        </li>
+        <li>
+          <Link to="/edit">Edit Expense</Link>
+        </li>
+        <li>
+          <Link to="/help">Help</Link>
+        </li>
+      </ul>
+
+      <hr />
+
       <Route exact path="/" component={ExpenseDashboardPage} />
-      <Route path="/create" component={AddExpensePage}/>
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
     </div>
-  </BrowserRouter>
+  </Router>
 );
 
 ReactDOM.render(routes, document.getElementById("app"));
