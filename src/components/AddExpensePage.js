433d0db9766f5e1380ce077f285de1a3567861da
sjon de gast <>
2019-04-19 17:02:19 +0200

    added ExpenseForm for create and edit, added onChange to track changes to input

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,4 +1,11 @@
-import React from "react";
-const AddExpensePage = () => <div>Add expense page</div>;
+import React from 'react';
+import ExpenseForm from './ExpenseForm';
+
+const AddExpensePage = () => (
+	<div>
+		<h1>Add expense</h1>
+		<ExpenseForm />
+	</div>
+);
 
 export default AddExpensePage;
