36d86fb860cb48be3da9ed84337536fb5e711772
sjon de gast <>
2019-04-20 17:05:00 +0200

    added react-dates datepicker and momentjs for time and installed libs

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,24 +1,35 @@
 import React from 'react';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
 
 export default class ExpenseForm extends React.Component {
 	state = {
 		description: '',
-        note: '',
-        amount: ''
+		note: '',
+        amount: '',
+        createdAt: moment(),
+        calendarFocused: false
 	};
 	onDescriptionChange = e => {
 		const description = e.target.value;
 		this.setState(() => ({ description }));
 	};
 	onNoteChange = e => {
-        e.persist();
+		e.persist();
 		this.setState(() => ({ note: e.target.value }));
+	};
+	onAmountChange = e => {
+		const amount = e.target.value;
+		if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+			this.setState(() => ({ amount }));
+		}
     };
-    onAmountChange = e => {
-        const amount = e.target.value;
-        if (amount.match(/^\d*(\.\d{0,2})?$/)) {
-            this.setState(() => ({ amount }));
-        } 
+    onDateChange = (createdAt) => {
+        this.setState(() => ({ createdAt }));
+    };
+    onFocusChange = ({ focused }) => {
+        this.setState(() => ({ calendarFocused: focused }));
     };
 	render() {
 		return (
@@ -31,7 +42,20 @@ export default class ExpenseForm extends React.Component {
 						value={this.state.description}
 						onChange={this.onDescriptionChange}
 					/>
-					<input type='text' placeholder='amount' value={this.state.amount} onChange={this.onAmountChange} />
+					<input
+						type='text'
+						placeholder='amount'
+						value={this.state.amount}
+						onChange={this.onAmountChange}
+					/>
+                    <SingleDatePicker 
+                        date={this.state.createdAt}
+                        onDateChange={this.onDateChange}
+                        focused={this.state.calendarFocused}
+                        onFocusChange={this.onFocusChange}
+                        numberOfMonths={1}
+                        isOutsideRange={() => false}
+                    />
 					<textarea
 						placeholder='Add a note for your expense (optional)'
 						value={this.state.note}
