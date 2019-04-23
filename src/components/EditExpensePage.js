91385a03895de0375de2778560c2218248eb978b
sjon de gast <>
2019-04-23 18:03:28 +0200

    setup edit expense page and tested it

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { editExpense } from '../actions/expenses';
 
-const EditExpensePage = (props) => {
-    console.log(props);
+const EditExpensePage = props => {
     return (
         <div>
-            This is the expense with the id: {props.match.params.id}
+            <ExpenseForm 
+                expense={props.expense}
+                onSubmit={(expense) => {
+                    props.dispatch(editExpense(props.expense.id, expense));
+                    props.history.push('/');
+                }}
+            />
         </div>
     );
 };
 
-export default EditExpensePage;
+const mapStateToProps = (state, props) => {
+	return {
+		expense: state.expenses.find(
+			expense => expense.id === props.match.params.id
+		)
+	};
+};
+
+export default connect(mapStateToProps)(EditExpensePage);
