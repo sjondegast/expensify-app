756545b969e753e06d0c22ad19cb3984fddf0e01
sjon de gast <>
2019-05-03 17:26:33 +0200

    removed toJson import from Header component

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from '../../components/Header';
 import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
 
 test('should test something', () => {
   const wrapper = shallow(<Header />);
