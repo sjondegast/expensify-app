90dc0f1c92eebfab2ae332a4008a0b4f2eb255ef
sjon de gast <>
2019-05-03 17:25:53 +0200

    added snapshotSerializer for enzyme to jest.config.json, this you don't have to pass toJson to every component to test it

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../../components/Header';
 import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 
 test('should test something', () => {
   const wrapper = shallow(<Header />);
-  expect(wrapper.find('h1').length).toBe(1);
+  expect(wrapper).toMatchSnapshot();
+
 });
 
 
