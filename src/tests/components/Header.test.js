9c790b398371f2ca04bc348db1877309c4612fff
sjon de gast <>
2019-05-03 14:12:24 +0200

    setup simple test Header component with enzyme and jest with configTest.js file

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
 import Header from '../../components/Header';
+import { shallow } from 'enzyme';
 
-test('should render header correctly', () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header />);
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
+test('should test something', () => {
+  const wrapper = shallow(<Header />);
+  expect(wrapper.find('h1').length).toBe(1);
 });
+
 
 
