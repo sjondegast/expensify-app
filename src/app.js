7a8bbacb7080b72e1ef6bb632842c8ae01965926
sjon de gast <>
2019-05-13 11:35:14 +0200

    removed store.subscribe, gonna connect database and remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -13,23 +13,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.subscribe(() => {
-	const state = store.getState();
-	const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-	console.log(visibleExpenses);
-});
-
-// addExpense
-store.dispatch(
-	addExpense({ description: 'Water bill', amount: 600, createdAt: 4500 })
-);
-store.dispatch(
-	addExpense({ description: 'Gas bill', amount: 1200, createdAt: 1000 })
-);
-store.dispatch(
-	addExpense({ description: 'Rent', amount: 1059, createdAt: 109500 })
-);
-
 const jsx = (
 	<Provider store={store}>
 		<AppRouter />
